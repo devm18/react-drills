c0eea9736668b710675c2c7a998aaa6f625134ea
John MacIsaac <johnmi@protonmail.com>
2018-09-07 17:19:39 -0500

    completed react-drills app1 and app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      foods: [ 'steak', 'heart', 'liver', 'tri-tip' ]
+    }
+  }
+
+  //// This can go here, but logically belongs within render() 
+  // foodsToDisplay() {
+  //   return this.state.foods.map((elem,i) => {
+  //     return (
+  //       <h3 key={i}> { elem } </h3>
+  //     )
+  //   });
+  // } 
+  //// and replace line 37 with its function call:  
+  // {/* <div> { this.foodsToDisplay() } </div> */}
+
+  
   render() {
+    let foodsToDisplay = this.state.foods.map((elem,i) => {
+      return (
+        <h3 key={i}> { elem } </h3>
+      )
+    }); 
+    
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>React-drills App-2</h2>
+        </div>    
+        <div> { foodsToDisplay } </div>
       </div>
     );
   }
